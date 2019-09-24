285127739511d20995324e9014440888b3307b9c
Your Name <you@example.com>
2019-09-24 18:42:51 +0200

    minor bug

--- js/index.js
+++ js/index.js
@@ -147,7 +147,7 @@ const printInfo = data => {
 
 // Function to store the input value
 let bringInfo =  () => {
-input = document.getElementById("username").value
+const input = document.getElementById("username").value
 // console.log(input)
 getUserInfo(input)
 
